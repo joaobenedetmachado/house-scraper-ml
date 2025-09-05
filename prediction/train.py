42994a923f4707f683bd34098e212fe6cd290863
joao machado <jvbm2177@gmail.com>
2025-09-05 20:42:30 -0300

    agora ele retorna os valores de acordo com cada modelo e tudo mais

---
+++ prediction/train.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from utils import train_price_models
+
+df = pd.read_csv('data/processed/data.csv')
+
+X = df[['nquartos','nbanheiros','ngaragem']]
+y = df['preco']
+
+
+models = train_price_models(X, y)
+
+print("\nmetricas dos modelos:")
+print(f"- Linear Regression -> R²: {models['linear_regression']['r2']:.2f}, MSE: {models['linear_regression']['mse']:.2f}")
+print(f"- Random Forest    -> R²: {models['random_forest']['r2']:.2f}, MSE: {models['random_forest']['mse']:.2f}")
+
+while True:
+    try:
+        nquartos = int(input("n quartos: "))
+        nbanheiros = int(input("n banheiros: "))
+        ngaragem = int(input("n garagem: "))
+    except ValueError:
+        print("somente numeros validos!\n")
+        continue
+
+    features = [[nquartos, nbanheiros, ngaragem]]
+
+    lr_price = models['linear_regression']['model'].predict(features)[0]
+    rf_price = models['random_forest']['model'].predict(features)[0]
+
+    print(f"\precos estimado:")
+    print(f"- Linear Regression: R$ {lr_price:,.2f}")
+    print(f"- Random Forest:    R$ {rf_price:,.2f}\n")
--- prediction/utils.py
+++ prediction/utils.py
@@ -2,6 +2,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+def train_price_models(X, y, test_size=0.2, random_state=42, n_estimators=100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    # aqui ele recebe os dados para treinar os modelos
+
+
+    lr_model = LinearRegression() # treina com regressao linear
+    lr_model.fit(X_train, y_train)
+    lr_pred = lr_model.predict(X_test)
+    lr_mse = mean_squared_error(y_test, lr_pred)
+    lr_r2 = r2_score(y_test, lr_pred)
+
+    # treina com o random forest regressor
+    rf_model = RandomForestRegressor(n_estimators=n_estimators, random_state=random_state)
+    rf_model.fit(X_train, y_train)
+    rf_pred = rf_model.predict(X_test)
+    rf_mse = mean_squared_error(y_test, rf_pred)
+    rf_r2 = r2_score(y_test, rf_pred)
+
+    return { # devolve as metricas 
+        'linear_regression': {'model': lr_model, 'mse': lr_mse, 'r2': lr_r2},
+        'random_forest': {'model': rf_model, 'mse': rf_mse, 'r2': rf_r2}
+    }
 
 def train_test_split_df(X, y, test_size=0.2, random_state=42):
     return train_test_split(X, y, test_size=test_size, random_state=random_state)
