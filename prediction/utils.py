6ba937db5919c6317601bcc69f6002e737485d8e
joao machado <jvbm2177@gmail.com>
2025-09-05 20:30:59 -0300

    agora arrumei pq a linearregresion nao usa accuracy

--- prediction/utils.py
+++ prediction/utils.py
@@ -1,36 +1,30 @@
-from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
 
-def get_accuracy_train_model_randomforest(X, y, test_size, estimators, random_state):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+def train_test_split_df(X, y, test_size=0.2, random_state=42):
+    return train_test_split(X, y, test_size=test_size, random_state=random_state)
 
-    rf = RandomForestClassifier(n_estimators=estimators, random_state=random_state)
-    rf.fit(X_train, y_train)
+def train_random_forest(X, y, test_size=0.2, n_estimators=100, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split_df(X, y, test_size, random_state)
+    model = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return accuracy_score(y_test, y_pred), model
 
-    y_pred = rf.predict(X_test)
+def train_logistic_regression(X, y, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split_df(X, y, test_size, random_state)
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    return accuracy_score(y_test, y_pred), model
 
-    return accuracy_score(y_test, y_pred)
-
-def get_accuracy_train_model_logisticregression(X, y, test_size, estimators, random_state):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-    clf = LogisticRegression(max_iter=200)
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-
-    return accuracy_score(y_test, y_pred)
-
-def get_accuracy_train_model_linearregression(X, y, test_size, estimators, random_state):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-
-    clf = LinearRegression()
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-
-    return accuracy_score(y_test, y_pred)
+def train_linear_regression(X, y, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split_df(X, y, test_size, random_state)
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    return mse, r2, model
